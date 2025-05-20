fd465872a493a1681ae95e935dca8131f7b5306f
Jaymie <29302715+LightCyan01@users.noreply.github.com>
2025-05-19 23:41:37 -0400

    Add shot management and collision detection for asteroids

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 print("Starting Asteroids!")
 print(f"Screen width: {SCREEN_WIDTH}")
@@ -20,9 +21,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     
     AsteroidField.containers = (updatable)
     asteroid_field = AsteroidField()
@@ -43,7 +46,11 @@ def main():
             if asteroid.collision_detect(player):
                 print("Game over!")
                 sys.exit()
-        
+            for shot in shots:
+                if shot.collision_detect(asteroid):
+                    asteroid.split()
+                    shot.kill()
+
         for obj in drawable:
             obj.draw(screen)
         
