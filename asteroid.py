888c42ebba83e0065e93dc6cdc4004239a6913fe
Jaymie <29302715+LightCyan01@users.noreply.github.com>
2025-05-19 23:41:30 -0400

    Add split method to Asteroid class for fragmenting asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,6 +12,21 @@ class Asteroid(CircleShape):
         
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+    
+    def split(self):
+        self.kill()
+        
+        if(self.radius <= ASTEROID_MIN_RADIUS):
+            return
+        else:
+            angle = random.uniform(20, 50)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid_1.velocity = pygame.math.Vector2.rotate(self.velocity, angle) * 1.2
+            asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid_2.velocity = pygame.math.Vector2.rotate(self.velocity, -angle) * 1.2
+            
+            
 
     
         
